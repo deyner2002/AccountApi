52d5b3f10f180cbd43e2c5e370faebb575080228
deyner2002 <solano27deiner@gmail.com>
2024-03-20 12:54:05 -0500

    Add gitignore

--- AccountApi/obj/Debug/net8.0/AccountApi.AssemblyInfo.cs
+++ AccountApi/obj/Debug/net8.0/AccountApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cba758946d4a759b19113076668a31207fdc141")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
